861b90212c72ded6af4bbeb57b84ec71226bfc20
Saurabh Singhvi <saurabhsinghvi@gmail.com>
2017-09-09 14:07:44 +0200

    Ready with tests

---
+++ src/XmlConstruct.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Sausin\XmlConstruct;
+
+use XMLWriter;
+use BadFunctionCallException;
+
+/**
+ * Credit goes to the commenters on php.net for the basic structure of this
+ * @see http://php.net/manual/en/ref.xmlwriter.php
+ */
+class XmlConstruct extends XMLWriter
+{
+    protected $separator;
+
+    /**
+     * Constructor.
+     *
+     * @param string $rootElementName
+     */
+    public function __construct(string $rootElementName, string $separator = '|')
+    {
+        $this->openMemory();
+        $this->setIndent(true);
+        $this->setIndentString('  ');
+        $this->startDocument('1.0', 'UTF-8');
+
+        $this->startElement($rootElementName);
+        $this->separator = $separator;
+    }
+
+    /**
+     * Construct elements and texts from an array. The array should contain an
+     * attribute's name in index part and a attribute's text in value part.
+     *
+     * @param  array  $prmArray Contains values
+     * @return XmlConstruct
+     */
+    public function fromArray(array $prmArray): XmlConstruct
+    {
+        foreach ($prmArray as $key => $val) {
+            if (is_array($val)) {
+                if (is_numeric($key)) {
+                    // numeric keys aren't allowed so we'll skip the key
+                    $this->fromArray($val);
+                } else {
+                    $this->writeKey($key);
+
+                    $this->fromArray($val);
+                    $this->endElement();
+                }
+            } else {
+                $this->setElement($key, $val);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return the content of a current xml document.
+     *
+     * @return string XML document
+     */
+    public function getDocument(): string
+    {
+        $this->endElement();
+        $this->endDocument();
+        
+        return $this->outputMemory();
+    }
+
+    /**
+     * Write a key.
+     *
+     * @param  string $key
+     * @return void
+     */
+    protected function writeKey(string $key): void
+    {
+        if (mb_strpos($key, $this->separator)) {
+            $pieces = explode($this->separator, $key);
+
+            // begin the element
+            $this->startElement(array_shift($pieces));
+
+            if (count($pieces) % 2 === 1) {
+                throw new BadFunctionCallException('Invalid attribute pair at '.end($pieces));
+            }
+
+            // write the attributes
+            foreach (array_chunk($pieces, 2) as [$attr, $val]) {
+                $this->writeAttribute($attr, $val);
+            }
+        } else {
+            // info($key);
+            $this->startElement($key);
+        }
+    }
+
+    /**
+     * Set an element with a text to a current xml document.
+     *
+     * @param  string $name An element's name
+     * @param  string $text An element's text
+     * @return void
+     */
+    protected function setElement(string $name, string $text): void
+    {
+        $this->writeKey($name);
+
+        $this->text($text);
+
+        $this->endElement();
+    }
+}
---
+++ tests/XmlConstructTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Sausin\XmlConstruct\Tests;
+
+use Sausin\XmlConstruct\XmlConstruct;
+
+// backward compatibility for phpunit
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+class XmlConstructTest extends \PHPUnit\Framework\TestCase
+{
+    /** @test */
+    public function it_can_generate_xml_from_a_plain_key_value_array()
+    {
+        $array = ['KEY1' => 'VALUE1', 'KEY2' => 'VALUE2'];
+
+        $output = (new XmlConstruct('ROOT'))->fromArray($array)
+                                            ->getDocument();
+
+        $compare = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL .
+                    '<ROOT>' . PHP_EOL .
+                    '  <KEY1>VALUE1</KEY1>' . PHP_EOL .
+                    '  <KEY2>VALUE2</KEY2>' . PHP_EOL .
+                    '</ROOT>' . PHP_EOL;
+
+        $this->assertEquals($compare, $output);
+    }
+
+    /** @test */
+    public function it_can_generate_xml_from_an_array_with_numeric_indices()
+    {
+        $array = [['KEY1' => 'VALUE1', 'KEY2' => 'VALUE2']];
+
+        $output = (new XmlConstruct('ROOT'))->fromArray($array)
+                                            ->getDocument();
+
+        $compare = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL .
+                    '<ROOT>' . PHP_EOL .
+                    '  <KEY1>VALUE1</KEY1>' . PHP_EOL .
+                    '  <KEY2>VALUE2</KEY2>' . PHP_EOL .
+                    '</ROOT>' . PHP_EOL;
+
+        $this->assertEquals($compare, $output);
+    }
+
+    /** @test */
+    public function it_can_generate_xml_with_attributes_from_an_array()
+    {
+        $array = [['KEY1|ATTR1|VAL1|ATTR2|VAL2' => 'VALUE1', 'KEY2' => 'VALUE2']];
+
+        $output = (new XmlConstruct('ROOT'))->fromArray($array)
+                                            ->getDocument();
+
+        $compare = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL .
+                    '<ROOT>' . PHP_EOL .
+                    '  <KEY1 ATTR1="VAL1" ATTR2="VAL2">VALUE1</KEY1>' . PHP_EOL .
+                    '  <KEY2>VALUE2</KEY2>' . PHP_EOL .
+                    '</ROOT>' . PHP_EOL;
+
+        $this->assertEquals($compare, $output);
+    }
+
+    /** @test */
+    public function it_can_generate_xml_with_attributes_from_an_array_with_custom_separator()
+    {
+        $array = [['KEY1:ATTR1:VAL1:ATTR2:VAL2' => 'VALUE1', 'KEY2' => 'VALUE2']];
+
+        $output = (new XmlConstruct('ROOT', ':'))->fromArray($array)
+                                            ->getDocument();
+
+        $compare = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL .
+                    '<ROOT>' . PHP_EOL .
+                    '  <KEY1 ATTR1="VAL1" ATTR2="VAL2">VALUE1</KEY1>' . PHP_EOL .
+                    '  <KEY2>VALUE2</KEY2>' . PHP_EOL .
+                    '</ROOT>' . PHP_EOL;
+
+        $this->assertEquals($compare, $output);
+    }
+
+    /** @test */
+    public function it_throws_an_error_if_incorrect_attribute_value_setup()
+    {
+        // array with missing value
+        $array = [['KEY1:ATTR1:VAL1:ATTR2' => 'VALUE1', 'KEY2' => 'VALUE2']];
+
+        $this->expectException('BadFunctionCallException');
+        $output = (new XmlConstruct('ROOT', ':'))->fromArray($array)
+                                            ->getDocument();
+    }
+
+    /** @test */
+    public function it_throws_an_error_if_value_associated_is_incorrect()
+    {
+        // array with missing value
+        $array = [['KEY1' => 'VALUE1', 'KEY2' => null]];
+
+        $this->expectException('TypeError');
+        $output = (new XmlConstruct('ROOT', ':'))->fromArray($array)
+                                            ->getDocument();
+    }
+}
